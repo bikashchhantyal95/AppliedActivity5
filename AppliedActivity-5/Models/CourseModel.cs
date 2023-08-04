bafd6e886b58937be4118ca7c97ec1481e76f7c2
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-08-03 22:04:30 -0400

    feat: Add Course model
    Adds the Course class representing a course entity in the database. The class contains fields for Name, Code, and CreditHours.

---
+++ AppliedActivity-5/Models/CourseModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppliedActivity_5.Models
+{
+	public class CourseModel
+	{
+        // Primary Key
+        [Key]
+        public int Id { get; set; }
+
+        // Course Name
+        [Required]
+        public string Name { get; set; }
+
+        // Course Code
+        [Required]
+        public string Code { get; set; }
+
+        // Course Credit Hours
+        [Required]
+        public int CreditHours { get; set; }
+    }
+}
+
